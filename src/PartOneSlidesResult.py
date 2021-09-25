a628962bdaf86638f313ff5e53a8fecbe98d9d86
Peiyi <peiyic03@gmail.com>
2021-09-25 11:31:09 -0400

    first pair of trajectories test for dtw

--- src/PartOneSlidesResult.py
+++ src/PartOneSlidesResult.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 
 Data_Highway1 = pandas.read_csv("../data/highway.csv")
 
-# Data_Highway = read_trajectories("../data/highway.csv")
+Data_Highway = read_trajectories("../data/highway.csv")
 #
-# Data_Highway
+print('Shape of data: {:}'.format(Data_Highway.shape))
+
+print('Getting first pair of trajectories: T (id=5), T_prime(id=7) from the highway.csv dataset in lane 1...')
+
+# id=5
+T_5 = Data_Highway[91:107]
+print('data for id=5:\n{:}\n'.format(T_5))
+
+# id=7
+T_7 = Data_Highway[125:143]
+print('data for id=5:\n{:}\n'.format(T_7))
+
+# get the cost and assignment for the first pair
+distance_dtw_pair_1, path_dtw_pair_1 = dtw(T_5[:,1:3], T_7[:,1:3])
+print('The dynamic time warping cost is: {:}'.format(distance_dtw_pair_1))
+print('The associated monotone assignment C is:\n{:}'.format(path_dtw_pair_1))
