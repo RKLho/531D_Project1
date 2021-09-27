3cb34f5a8b523326a3a490f2743279149d59769d
Peiyi <peiyic03@gmail.com>
2021-09-26 22:14:36 -0400

    step 1-2 in slides

--- src/Part1.py
+++ src/Part1.py
@@ -25,7 +25,6 @@ def dtw(T1, T2):
         for j in range(m+1):
             my_matrix[i, j] = np.inf
     my_matrix[0][0] = 0
-#     path = np.array([1, 1])
     for i in range(1, n+1):
         for j in range(1, m+1):
             cost = euclidean_square(T1[i-1,:], T2[j-1,:])
@@ -37,53 +36,26 @@ def dtw(T1, T2):
             else:
                 my_matrix[i][j] = cost + my_matrix[i-1][j-1]
     warp_path = findPath(my_matrix)
-    # warp_path = traceback(my_matrix)
+
     return my_matrix[n][m], warp_path
 
+
 def findPath(my_matrix):
-    n, m = my_matrix.shape
-    i = n-1
-    j = m-1
-
+    # index starts from 0
+    i, j = np.array(my_matrix.shape) - 2
     path = np.array([i, j])
-
-    while i > 1 or j > 1:
-        if i == 1:
-            path = np.vstack([path, np.array([i, j-1])])
+    while i > 0 or j > 0:
+        index = np.argmin((my_matrix[i, j], my_matrix[i, j + 1], my_matrix[i + 1, j]))
+        if index == 0:
+            i = i-1
             j = j-1
-        elif j == 1:
-            path = np.vstack([path, np.array([i-1, j])])
+        elif index == 1:
             i = i-1
         else:
-            index_min = np.argmin([my_matrix[i-1][j], my_matrix[i-1][j-1], my_matrix[i][j-1]])
-            if index_min == 0:
-                path = np.vstack([path, np.array([i-1, j])])
-                i = i-1
-            elif index_min == 1:
-                path = np.vstack([path, np.array([i-1, j-1])])
-                j = j-1
-                i = i-1
-            else:
-                path = np.vstack([path, np.array([i, j-1])])
-                j = j-1
+            j = j-1
+        path = np.vstack([path, np.array([i, j])])
 
-    return path-1
-
-def traceback(D):
-    i, j = np.array(D.shape) - 2
-    p, q = [i], [j]
-    while (i > 0) or (j > 0):
-        tb = np.argmin((D[i, j], D[i, j + 1], D[i + 1, j]))
-        if tb == 0:
-            i -= 1
-            j -= 1
-        elif tb == 1:
-            i -= 1
-        else:  # (tb == 2):
-            j -= 1
-        p.insert(0, i)
-        q.insert(0, j)
-    return np.array(p), np.array(q)
+    return path
 
 def dfd(T1, T2):
     n = len(T1)
@@ -117,30 +89,3 @@ def findDfdPath(my_matrix):
     path = findPath(pad_m)
 
     return path
-
-
-# In[3]:
-
-
-# x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
-# y = np.array([[2,2], [3,3], [4,4]])
-# my_distance_dtw, my_path_dtw = dtw(x, y)
-# print(my_distance_dtw)
-# print(my_path_dtw)
-
-
-# In[4]:
-
-
-# my_distance_dfd, my_path_dfd = dfd(x, y)
-# print(my_distance_dfd)
-# print(my_path_dfd)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
--- src/Part1SlidesResult.py
+++ src/Part1SlidesResult.py
@@ -3,61 +3,57 @@ from tools import *
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
-from fastdtw import fastdtw
-from scipy.spatial.distance import euclidean
 
-Data_Highway1 = pandas.read_csv("../data/highway.csv")
+# Data_Highway1 = pandas.read_csv("../data/highway.csv")
 
 Data_Highway = read_trajectories("../data/highway.csv")
 #
 print('Shape of data: {:}'.format(Data_Highway.shape))
 
-print('Getting first pair of trajectories: T (id=5), T_prime(id=7) from the highway.csv dataset in lane 1...')
+print('Getting first pair of trajectories: T (id=5), T_prime(id=16) from the highway.csv dataset in lane 1...')
 
 # id=5
 T_5 = Data_Highway[91:107]
-print('data for id=5:\n{:}\n'.format(T_5))
+print('data for id = 5:\n{:}\n'.format(T_5))
 
-# id=16
-T_16 = Data_Highway[276:291]
-print('data for id=16:\n{:}\n'.format(T_16))
+# id=18
+T_18 = Data_Highway[308:323]
+print('data for id = 18:\n{:}\n'.format(T_18))
 
 # get the cost and assignment for the first pair
-distance_dtw_pair_1, path_dtw_pair_1 = dtw(T_5[:,1:3], T_16[:,1:3])
+distance_dtw_pair_1, path_dtw_pair_1 = dtw(T_5[:,1:3], T_18[:,1:3])
 print('The dynamic time warping (DTW) cost is: {:}'.format(distance_dtw_pair_1))
 print('The associated monotone assignment C is:\n{:}\n'.format(path_dtw_pair_1))
 
-# distance, path = fastdtw(T_5[:,1:3], T_16[:,1:3], dist=euclidean)
-# print(path)
-# print(distance)
-
 # get the cost and assignment for the first pair
-distance_dfd_pair_1, path_dfd_pair_1 = dfd(T_5[:,1:3], T_16[:,1:3])
+distance_dfd_pair_1, path_dfd_pair_1 = dfd(T_5[:,1:3], T_18[:,1:3])
 print('The discrete Fréchet distance (DFD) cost is: {:}'.format(distance_dfd_pair_1))
 print('The associated monotone assignment C is:\n{:}\n'.format(path_dfd_pair_1))
 
 # draw results for first path
-draw_assignment(T_5[:,1:3], T_16[:,1:3], path_dtw_pair_1, 'dtw (id=5, id=16)', '../images/part1/dtw_5_16')
-draw_assignment(T_5[:,1:3], T_16[:,1:3], path_dfd_pair_1, 'dfd (id=5, id=16)', '../images/part1/dfd_5_16')
+draw_assignment(T_5[:,1:3], T_18[:,1:3], path_dtw_pair_1, 'dtw (id=5, id=18, cost={:})'.format(distance_dtw_pair_1), '../images/part1/dtw_5_18')
+draw_assignment(T_5[:,1:3], T_18[:,1:3], path_dfd_pair_1, 'dfd (id=5, id=18, cost={:})'.format(distance_dfd_pair_1), '../images/part1/dfd_5_18')
+
+
+
 
 # next pair
-T_18 = Data_Highway[308:323]
-print('data for id=18:\n{:}\n'.format(T_5))
+T_28 = Data_Highway[481:496]
+print('data for id=18:\n{:}\n'.format(T_28))
 
 T_22 = Data_Highway[374:390]
-print('data for id=22:\n{:}\n'.format(T_16))
+print('data for id=22:\n{:}\n'.format(T_22))
 #
 # get the cost and assignment for the first pair
-distance_dtw_pair_2, path_dtw_pair_2 = dtw(T_18[:,1:3], T_22[:,1:3])
+distance_dtw_pair_2, path_dtw_pair_2 = dtw(T_28[:,1:3], T_22[:,1:3])
 print('The dynamic time warping (DTW) cost is: {:}'.format(distance_dtw_pair_2))
 print('The associated monotone assignment C is:\n{:}\n'.format(path_dtw_pair_2))
 
-
 # get the cost and assignment for the first pair
-distance_dfd_pair_2, path_dfd_pair_2 = dfd(T_18[:,1:3], T_22[:,1:3])
+distance_dfd_pair_2, path_dfd_pair_2 = dfd(T_28[:,1:3], T_22[:,1:3])
 print('The discrete Fréchet distance (DFD) cost is: {:}'.format(distance_dfd_pair_2))
 print('The associated monotone assignment C is:\n{:}\n'.format(path_dfd_pair_2))
 #
 # # draw results for first path
-draw_assignment(T_18[:,1:3], T_22[:,1:3], path_dtw_pair_2, 'dtw (id=18, id=22)', '../images/part1/dtw_18_22')
-draw_assignment(T_18[:,1:3], T_22[:,1:3], path_dfd_pair_2, 'dfd (id=18, id=22)', '../images/part1/dfd_18_22')
+draw_assignment(T_28[:,1:3], T_22[:,1:3], path_dtw_pair_2, 'dtw (id=28, id=22, cost={:})'.format(distance_dtw_pair_2), '../images/part1/dtw_28_22')
+draw_assignment(T_28[:,1:3], T_22[:,1:3], path_dfd_pair_2, 'dfd (id=28, id=22, cost={:})'.format(distance_dfd_pair_2), '../images/part1/dfd_28_22')
