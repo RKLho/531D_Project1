5436bd57def3535018ec6eeee44df0a092aa3214
Peiyi <peiyic03@gmail.com>
2021-09-26 22:41:59 -0400

    clean code

--- src/Part1.py
+++ src/Part1.py
@@ -19,6 +19,7 @@ def euclidean_square(p1, p2):
 def dtw(T1, T2):
     n = len(T1)
     m = len(T2)
+    # use dynamic programming
     my_matrix = np.zeros((n+1, m+1))
 
     for i in range(n+1):
@@ -28,13 +29,13 @@ def dtw(T1, T2):
     for i in range(1, n+1):
         for j in range(1, m+1):
             cost = euclidean_square(T1[i-1,:], T2[j-1,:])
-            index_min = np.argmin([my_matrix[i-1][j], my_matrix[i][j-1], my_matrix[i-1][j-1]])
+            index_min = np.argmin([my_matrix[i-1][j-1], my_matrix[i-1][j], my_matrix[i][j-1]])
             if index_min == 0:
-                my_matrix[i][j] = cost + my_matrix[i-1][j]
-            elif index_min == 1:
-                my_matrix[i][j] = cost + my_matrix[i][j-1]
-            else:
                 my_matrix[i][j] = cost + my_matrix[i-1][j-1]
+            elif index_min == 1:
+                my_matrix[i][j] = cost + my_matrix[i-1][j]
+            else:
+                my_matrix[i][j] = cost + my_matrix[i][j-1]
     warp_path = findPath(my_matrix)
 
     return my_matrix[n][m], warp_path
@@ -61,28 +62,28 @@ def dfd(T1, T2):
     n = len(T1)
     m = len(T2)
 
-    ca = np.ones((n, m))*-1
+    # -1 as unvisited
+    my_matrix = np.ones((n, m))*-1
 
-    def _c(i, j):
-        if ca[i][j] > -1:
-            ca[i][j] = ca[i][j]
+    def search(i, j):
+        # if already visited, return the value
+        if my_matrix[i][j] > -1:
+            my_matrix[i][j] = my_matrix[i][j]
         elif i == 0 and j == 0:
-            ca[i][j] = euclidean_square(T1[0, :], T2[0, :])
+            my_matrix[i][j] = euclidean_square(T1[0, :], T2[0, :])
         elif i > 0 and j == 0:
-            ca[i][j] = max(_c(i-1, 0), euclidean_square(T1[i, :], T2[0, :]))
+            my_matrix[i][j] = max(search(i-1, 0), euclidean_square(T1[i, :], T2[0, :]))
         elif i == 0 and j > 0:
-            ca[i][j] = max(_c(0, j-1), euclidean_square(T1[0,:], T2[j,:]))
+            my_matrix[i][j] = max(search(0, j-1), euclidean_square(T1[0,:], T2[j,:]))
         elif i > 0 and j > 0:
-            ca[i][j] = max(min(_c(i-1,j), _c(i-1,j-1),_c(i,j-1)), euclidean_square(T1[i, :], T2[j, :]))
-        else:
-            ca[i][j] = np.inf
-        return ca[i][j]
+            my_matrix[i][j] = max(min(search(i-1,j), search(i-1,j-1),search(i,j-1)), euclidean_square(T1[i, :], T2[j, :]))
+        return my_matrix[i][j]
 
-    return _c(n-1, m-1), findDfdPath(ca)
+    return search(n-1, m-1), findDfdPath(my_matrix)
 
 def findDfdPath(my_matrix):
     n, m = my_matrix.shape
-    # pad
+    # pad to reuse findPath
     pad_m = np.hstack([np.ones((n,1))*np.inf, my_matrix])
     pad_m = np.vstack([np.ones((1,m+1))*np.inf, pad_m])
 
